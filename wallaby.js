80fca83d169195fb4f8c7c113a3a464183b593a3
Alex Malkevich <malkevich.alex@gmail.com>
2015-09-19 04:35:16 +0400

    Connect and check Wallaby TDD test runner

---
+++ wallaby.js
@@ -0,0 +1,22 @@
+module.exports = function (w) {
+
+    return {
+        files: [
+            'src/**/*.ts'
+        ],
+
+        tests: [
+            'test/**/*Spec.ts'
+        ]
+
+        // TypeScript compiler is on by default with default options,
+        // you can configure built-in compiler by passing options to it
+        // See interface CompilerOptions in
+        // https://github.com/Microsoft/TypeScript/blob/master/src/compiler/types.ts
+        //compilers: {
+
+        //  '**/*.ts': w.compilers.typeScript({})
+        //}
+
+    };
+};
